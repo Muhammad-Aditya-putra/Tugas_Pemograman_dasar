0414245ae95d19aa2e193ccc91f0a1267f815bb0
Muhammad-Aditya-putra <cellutama814@gmail.com>
2025-11-30 08:46:39 +0800

    Implement student information using a dictionary
    This script demonstrates the use of a dictionary to store and print student information including name, NIM, and program of study.

---
+++ penerapan Dictionary.py
@@ -0,0 +1,19 @@
+#Nama : Muhammad Aditya Putra
+#NIM : D0425516
+#Prodi : Sistem Informasi
+#Tugas Penerapan Dictionary pada, Nama,Nim dan Prodi
+
+#data
+
+mahasiswa={
+	"Nama" : "Muhammad Aditya Putra",
+	"NIM" : "D0425516",
+	"Prodi" : "Sistem Informasi"
+}
+print(mahasiswa["Nama"])
+print(mahasiswa["NIM"])
+print(mahasiswa["Prodi"])
+
+# ( Penjelasan ) Dictionary adalah struktur data yang digunakan untuk menyimpan data dalam bentuk pasangan kunci (key) dan nilai (value).
+
+# ( Penjelasan ) Berbeda dengan array atau list yang menggunakan indeks angka, dictionary menggunakan key (kata unik) untuk mengakses data.
