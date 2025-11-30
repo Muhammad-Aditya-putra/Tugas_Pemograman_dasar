59262b23a21d3e730525719ba760151219958191
Muhammad-Aditya-putra <cellutama814@gmail.com>
2025-11-30 08:43:51 +0800

    Add script to calculate square area

---
+++ menghitung luas persegi.py
@@ -0,0 +1,11 @@
+#Nama : Muhammad Aditya Putra
+#NIM : D0425516
+#Tugas menghitung luas persegi
+
+sisi = float(input("MASUKAN SISI PERSEGI :  "))
+
+sisi = float(input("MASUKAN SISI PERSEGI : "))
+
+luas = ( sisi * sisi) 
+
+print(f"LUAS PERSEGI ADALAH : {luas}")
