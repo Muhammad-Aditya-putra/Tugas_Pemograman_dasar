ebddee3f4b9adb5c943478e1e58a99ec14a8faa0
Muhammad-Aditya-putra <cellutama814@gmail.com>
2025-11-30 08:42:34 +0800

    Add triangle area calculation script

---
+++ menghitung luas segitiga.py
@@ -0,0 +1,11 @@
+#Nama : Muhammad Aditya putra
+#NIM : D0425516
+#Tugas membuat luas segitiga
+
+alas = float(input("MASUKAN ALAS SEGITIGA :  "))
+
+tinggi = float(input("MASUKAN TINGGI SEGITIGA : "))
+
+luas = (alas * tinggi) / 2
+
+print(f"LUAS SEGITIGA ADALAH : {luas}")
