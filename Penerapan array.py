21b6804f6ae22cc306beb6fe5d9af94651f37f7a
Muhammad-Aditya-putra <cellutama814@gmail.com>
2025-11-30 08:44:46 +0800

    Add array implementation for student data
    This script demonstrates the use of an array to store and print student information.

---
+++ Penerapan array.py
@@ -0,0 +1,15 @@
+#Nama : Muhammad Aditya Putra
+#NIM : D0425516
+#Prodi : Sistem Informasi
+#Tugas Penerapan Array pada, Nama,Nim dan Prodi
+
+#data
+
+data =["Muhammad Aditya Putra","D0425516","Sistem Informasi"]
+print(data[0])
+print(data[1])
+print(data[2])
+
+# ( Penjelasan ) Array adalah struktur data yang digunakan untuk menyimpan sekumpulan nilai atau elemen dengan tipe data yang sama dalam satu variabel, dan setiap elemen dapat diakses menggunakan indeks (nomor urut).
+
+# ( Penjelasan ) Array berfungsi seperti wadah yang menampung banyak data sejenis (misalnya semua angka, semua huruf, atau semua string).
